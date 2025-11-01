b6d046d807f9e7c0bd414ee8e0cfcce1d9496dcf
Jani Nikula <jani@nikula.org>
2025-11-01 19:20:41 +0200

    Initial content drop

---
+++ doc/conf.py
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# SPDX-FileCopyrightText: 2025 Jani Nikula <jani@nikula.org>
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Snooker League'
+copyright = '2025, Jani'
+author = 'Jani'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,12 @@
+.. SPDX-License-Identifier: AGPL-3.0-or-later
+.. SPDX-FileCopyrightText: 2025 Jani Nikula <jani@nikula.org>
+
+Snooker League
+==============
+
+Hello.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
